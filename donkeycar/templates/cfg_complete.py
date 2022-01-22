399fc460130fae9f18e2d3e765aa0d67a9aa35a8
Kristjan Roosild <kristjan.roosild@wise.com>
2022-01-22 19:14:07 +0200

    add missing config

--- donkeycar/templates/cfg_complete.py
+++ donkeycar/templates/cfg_complete.py
@@ -117,6 +117,7 @@ HBRIDGE_L298N_PIN_RIGHT_EN = 11
 #TRAINING
 
 ADD_MIRROR = False # should we double the training data by adding mirrored images/angles on the fly?
+FLIP_LEFT_RIGHT = False
 
 # The default AI framework to use. Choose from (tensorflow|pytorch)
 DEFAULT_AI_FRAMEWORK = 'tensorflow'
