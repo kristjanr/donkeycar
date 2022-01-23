94de9851f96b22e2ba1caad5b476bab331185067
Kristjan Roosild <kristjan.roosild@wise.com>
2022-01-23 16:37:08 +0200

    understanding

--- donkeycar/parts/throttle_filter.py
+++ donkeycar/parts/throttle_filter.py
@@ -1,3 +1,5 @@
+import logging
+
 
 class ThrottleFilter(object):
     '''
@@ -9,19 +11,32 @@ class ThrottleFilter(object):
         self.last_throttle = 0.0
 
     def run(self, throttle_in):
+        v = dict(
+            throttle_in=throttle_in,
+            reverse_triggered=self.reverse_triggered,
+            last_throttle=self.last_throttle
+        )
+        logging.info("ThrottleFilter Start", v)
         if throttle_in is None:
             return throttle_in
 
         throttle_out = throttle_in
 
-        if throttle_out < 0.0:
-            if not self.reverse_triggered and self.last_throttle < 0.0:
-                throttle_out = 0.0
-                self.reverse_triggered = True
+        if throttle_out < 0.0: # hetkel tagurdab
+            if not self.reverse_triggered and self.last_throttle < 0.0: # ei ole hetkel triggerdatud ja eelnevalt tagurdas
+                throttle_out = 0.0 # jää seisma
+                self.reverse_triggered = True # triggerda tagurdamine
         else:
             self.reverse_triggered = False
 
         self.last_throttle = throttle_out
+
+        v = dict(
+            throttle_out=throttle_out,
+            reverse_triggered=self.reverse_triggered,
+            last_throttle=self.last_throttle
+        )
+        logging.info("ThrottleFilter End", v)
         return throttle_out
 
     def shutdown(self):
