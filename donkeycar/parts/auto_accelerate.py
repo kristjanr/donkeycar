861f94e0140154ca99db02456c10f7f5dcf2324c
Kristjan Roosild <kristjan.roosild@wise.com>
2022-01-23 17:13:40 +0200

    implement and log

--- donkeycar/parts/auto_accelerate.py
+++ donkeycar/parts/auto_accelerate.py
@@ -1,20 +1,35 @@
 import logging
 
+import numpy as np
+
 
 class AutoAccelerate(object):
     def __init__(self):
-        pass
+        self.last_20_images = [np.zeros((120, 160, 3)) for _ in range(20)]
+        self.counter = 0
 
     def run(self, throttle_in, mode, image):
-        img_shp = image.shape if image is not None else None
+        if mode != 'local_angle' or image is None:
+            return throttle_in
+
+        self.counter += 1
+        self.last_20_images.append(image)
+        self.last_20_images.pop(0)
+
+        if self.counter % 20 != 0:
+            return throttle_in
+
+        throttle_out = throttle_in
+
         v = dict(
             throttle_in=throttle_in,
             mode=mode,
-            image_shp=img_shp
+            first_10_avg=np.array(self.last_20_images[:10]).mean(),
+            last_10_avg=np.array(self.last_20_images[10:]).mean()
         )
-        logging.info(f"AutoAccelerate {v}")
 
-        return throttle_in
+        logging.info(f"AutoAccelerate out {throttle_out}")
+        return throttle_out
 
     def shutdown(self):
         pass
