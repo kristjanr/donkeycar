4d40b4bd3d2ddfb92f058ee6e9a49ddfbd7d2acc
Kristjan Roosild <kristjan.roosild@wise.com>
2022-01-23 19:53:32 +0200

    tweaks

--- donkeycar/parts/auto_reverse.py
+++ donkeycar/parts/auto_reverse.py
@@ -14,6 +14,10 @@ class AutoReverse(object):
 
     def run(self, mode, constant_throttle, image):
         if mode != 'local_angle' or image is None or not constant_throttle:
+            self.counter = 0
+            self.stopped_counter = 0
+            self.reversing = False
+            self.reverse_counter = 0
             return False
 
         self.counter += 1
